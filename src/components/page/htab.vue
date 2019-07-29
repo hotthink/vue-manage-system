6d21bcf4a0a30df72bb49554fc90aaf21876300f
hotthink <fangle77@163.com>
2019-07-29 22:16:17 +0800

    常规更新

--- src/components/page/htab.vue
+++ src/components/page/htab.vue
@@ -5,19 +5,34 @@
 </template>
 
 <script>
-  import HotTable from 'vue-handsontable-official';
-  export default {
-    data: function() {
-      return {
-        root: 'test-hot',
-        hotSettings: {
-          data: [['sample', 'data']],
-          colHeaders: true
-        }
-      };
-    },
-    components: {
-      HotTable
-    }
+import HotTable from "vue-handsontable-official";
+export default {
+  data: function() {
+    return {
+      root: "test-hot",
+      hotSettings: {
+        data: HotTable.helper.createSpreadsheetData(1000, 1000),
+        width: 684,
+        height: 720,
+        colWidths: 100,
+        rowHeights: 23,
+        rowHeaders: true,
+        colHeaders: true,
+        fixedRowsTop: 2,
+        fixedColumnsLeft: 2,
+        manualColumnMove: true,
+        manualRowMove: true,
+        dropdownMenu: true,
+        contextMenu: true,
+        manualColumnResize: true,
+        manualRowResize: true,
+        language: "zh-CN",
+        licenseKey: "non-commercial-and-evaluation"
+      }
+    };
+  },
+  components: {
+    HotTable
   }
+};
 </script>
